a81256ddb37ac865f2a271768d8a8af61736383f
nicocorbi <nicolascorbi2002@gmail.com>
2025-04-04 14:27:16 +0200

    dia 2 freno de mano

--- Assets/scripts/VehicleMovement.cs
+++ Assets/scripts/VehicleMovement.cs
@@ -1,59 +1,156 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
 
-public class VehicleMovement3D : MonoBehaviour
+public class Movement : MonoBehaviour
 {
-    public float acceleration = 10f;
-    public float maxSpeed = 5f;
-    public float friction = 2f;
-    public float turnSpeed = 50f;
-
-    private Vector3 velocity = Vector3.zero;
+    [SerializeField] float speed;
+    [SerializeField] float maxSpeed;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] float BrakeManorotationSpeed;
+    [SerializeField] float maxRotationSpeed;
+    [SerializeField] float friction;
+    [SerializeField] float brakeForce;
+    [SerializeField] float frenoDeMano;
+    private TrailRenderer trail;
     private Rigidbody rb;
+
+    private bool accelerating;
+    private bool decelerating;
+    private bool frenoMano;
+
+    private float rotationInput;
+    private float rotationFrenoManoInput;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        trail = GetComponent<TrailRenderer>();
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.AddTorque(Vector3.up * turnSpeed * Time.deltaTime, ForceMode.VelocityChange);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            rb.AddTorque(Vector3.down * turnSpeed * Time.deltaTime, ForceMode.VelocityChange);
-        }
+        movementWithInput();
     }
 
     void FixedUpdate()
     {
-        ApplyMovement();
+        MovementPhysics();
     }
 
-    void ApplyMovement()
+    
+    void movementWithInput()
     {
-        Vector3 inputMovement = Vector3.zero;
+        rotationInput = 0;
 
-        if (Input.GetKey(KeyCode.W)) inputMovement -= transform.up;
-        if (Input.GetKey(KeyCode.S)) inputMovement += transform.up;
+        accelerating = Input.GetKey(KeyCode.W);
 
-        inputMovement = inputMovement.normalized;
+        decelerating = Input.GetKey(KeyCode.S);
 
-        if (inputMovement.magnitude > 0)
+        if (Input.GetKey(KeyCode.A))
         {
-            velocity += inputMovement * acceleration * Time.fixedDeltaTime;
-            velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+            rotationInput = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rotationInput = 1;
+        }
+        frenoMano = Input.GetKey(KeyCode.Space);
+        
+
+
+    }
+
+    // Controls the movement physics and applies friction
+    void MovementPhysics()
+    {
+        rb.AddTorque(rotationInput * rotationSpeed * transform.up * Time.fixedDeltaTime);
+       
+
+
+        Vector3 forwardDirection = transform.forward; 
+
+        Vector3 localVelocity = transform.InverseTransformDirection(rb.linearVelocity); 
+
+        if (accelerating)
+        {
+            rb.linearVelocity += speed * forwardDirection * Time.fixedDeltaTime;
         }
         else
         {
-            velocity = Vector3.Lerp(velocity, Vector3.zero, friction * Time.fixedDeltaTime);
+            // Apply friction to gradually reduce speed
+            rb.linearVelocity = new Vector3(
+                rb.linearVelocity.x * (1 / (1 + friction * Time.fixedDeltaTime)),
+                0,
+                rb.linearVelocity.z * (1 / (1 + friction * Time.fixedDeltaTime))
+            );
         }
 
-        rb.linearVelocity = velocity;
+        if (decelerating)
+        {
+            if (localVelocity.z > 0) 
+            {
+                Brake(); 
+            }
+            else 
+            {
+                rb.linearVelocity -= speed * forwardDirection * Time.fixedDeltaTime;
+            }
+        }
+        if (frenoMano)
+        {
+            if (localVelocity.z > 0)
+            {
+                // aplico freno
+                BrakeMano();
+
+                // aplico giro adicional
+                rb.AddTorque(rotationInput * BrakeManorotationSpeed * transform.up * Time.fixedDeltaTime);
+                
+
+            }
+            else
+            {
+                rb.linearVelocity -= speed * forwardDirection * Time.fixedDeltaTime;
+            }
+        }
+
+       
+        if (rb.linearVelocity.magnitude > maxSpeed)
+        {
+            rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+        }
+        
+        if (rb.angularVelocity.magnitude > maxRotationSpeed)
+        {
+            rb.angularVelocity = rb.angularVelocity.normalized * maxRotationSpeed;
+        }
+    }
+
+    void Brake()
+    {
+        rb.linearVelocity = new Vector3(
+            rb.linearVelocity.x * (1 / (1 + brakeForce * Time.fixedDeltaTime)),
+            0,
+            rb.linearVelocity.z * (1 / (1 + brakeForce * Time.fixedDeltaTime))
+        );
+    }
+    void BrakeMano()
+    {
+        rb.linearVelocity = new Vector3(
+            rb.linearVelocity.x * (1 / (1 + frenoDeMano * Time.fixedDeltaTime)),
+            0,
+            rb.linearVelocity.z * (1 / (1 + frenoDeMano * Time.fixedDeltaTime))
+        );
+
+
     }
 }
+
+
+
 
 
 
